7e088e0458e9b465c71d17eb675cbc9d56b9b762
Maksim Erekhinskii <max.erekhinskiy@gmail.com>
2026-07-20 09:08:13 +0200

    vitepress: add Vercel Web Analytics

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,11 @@
+import { inject } from '@vercel/analytics';
 import DefaultTheme from 'vitepress/theme';
 import './custom.css';
+
+if (typeof window !== 'undefined') {
+  inject({
+    mode: import.meta.env.DEV ? 'development' : 'production',
+  });
+}
 
 export default DefaultTheme;
